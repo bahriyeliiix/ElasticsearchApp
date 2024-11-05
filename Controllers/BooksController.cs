d0f8e579b83bd23cf840ca6965eb835efab1d658
DELL <DELL@DESKTOP-4RGAC49>
2024-11-05 15:41:47 +0300

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,89 @@
+﻿using Business.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticsearchApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ElasticSearchService _elasticSearchService;
+
+        public BooksController(ElasticSearchService elasticSearchService)
+        {
+            _elasticSearchService = elasticSearchService;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _elasticSearchService.GetAllBooks();
+            return View(result);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            book.Id = Guid.NewGuid().ToString();
+            _elasticSearchService.IndexDocument(book);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(string id)
+        {
+            var book = _elasticSearchService.GetBook(id);
+            return View(book);
+        }
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var book = _elasticSearchService.GetBook(id);
+            if (book != null)
+            {
+                _elasticSearchService.DeleteBook(id); 
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var book = _elasticSearchService.GetBook(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var book = _elasticSearchService.GetBook(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _elasticSearchService.IndexDocument(book);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Business.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ElasticsearchService'i ekleyin
+builder.Services.AddSingleton<ElasticSearchService>();
+
+// Diğer servisleri ekleyin
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Elasticsearch indeksi oluşturma
+using (var scope = app.Services.CreateScope())
+{
+    var elasticsearchService = scope.ServiceProvider.GetRequiredService<ElasticSearchService>();
+    elasticsearchService.CreateIndexIfNotExists();
+}
+
+// Middleware'leri ekleyin
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
